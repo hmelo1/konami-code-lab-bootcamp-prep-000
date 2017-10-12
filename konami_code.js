250522a0069c0393c28d22fda289d66951217070
hmelo1 <heriberto@melo.nyc>
2017-10-12 02:42:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,31 +1,42 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-const body = document.querySelector('body')
 
-body.addEventListener('keydown', function(e){
-  key = parseInt(e.detail)
-  if (key === code[0]){
-    alert("Yay!")
-  }
-})
+
+
 
 function init() {
   // Write your JavaScript code inside the init() function
 
 }
 
+
+const alphabet = [65, 66, 67]
+const input = document.querySelector('input')
+let index = 0;
+
 function onKeyDownHandler(e){
-  const key = parseInt(e.detail || e.key)
+  const key = parseInt(e.detail || e.which)
 
-  if (key === code[index]){
-    code++
-    alert("Test111")
+  if (key === alphabet[index]){
+    index++;
 
-    if (index === code.length){
-      alert("Test222")
+    if (index === alphabet.length){
+      alert("Hurray!")
 
-      index = 0;
+      index = 0
     }
-  }else {
-    index = 0;
+  }else{
+    index = 0
   }
 }
+
+input.addEventListener('keydown', onKeyDownHandler())
+
+const input = document.querySelector('input')
+
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0
+input.addEventListener('keydown', function(e) {
+  if (e.detail === code[0]){
+    alert("Yay!")
+  }
+})
