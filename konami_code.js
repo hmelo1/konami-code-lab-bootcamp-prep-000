c064f2d9612ae9f7bb54405b99341c751f044401
hmelo1 <heriberto@melo.nyc>
2017-10-12 02:12:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const body = document.querySelector('body')
 
+body.addEventListener('keydown', function(e){
+  function onKeyDownHandler(e){
+    const key = parseInt(e.detail || e.key)
+
+    if (key === code[index]){
+      code++
+      alert("Test111")
+
+      if (index === code.length){
+        alert("Test222")
+
+        index = 0;
+      }
+    }else {
+      index = 0;
+    }
+  }
+	console.log(e.which)
+})
 
 function init() {
   // Write your JavaScript code inside the init() function
@@ -24,6 +43,3 @@ function onKeyDownHandler(e){
   }
 }
 
-body.addEventListener('keydown', onKeyDownHandler(e){
-	console.log(e.which)
-})
