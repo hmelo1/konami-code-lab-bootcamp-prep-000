1120b496d1dba3993916c573d7cb928cdc715a05
hmelo1 <heriberto@melo.nyc>
2017-10-12 10:59:38 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,45 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+const body = document.querySelector('body')
 
 
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let index = 0;
 
-}
+  // This is the function that would be invoked by the event listener.
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which); //convert the key inputed into a integer
 
+    if (key === code[index]) { // compare key var with the code
+      index++;
 
-const alphabet = [65, 66, 67]
-const input = document.querySelector('input')
-let index = 0;
+      if (index === code.length) { //if the index is as long as the array length
+        alert("Hurray!");
 
-function onKeyDownHandler(e){
-  const key = parseInt(e.detail || e.which)
-
-  if (key === alphabet[index]){
-    index++;
-
-    if (index === alphabet.length){
-      alert("Hurray!")
-
-      index = 0
-    }
-  }else{
-    index = 0
-  }
-}
-
-input.addEventListener('keydown', onKeyDownHandler())
-
-const input = document.querySelector('input')
-
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0
-input.addEventListener('keydown', function(e) {
-  for (i = 0; i < code.length; i++){
-    if (parseInt(e.which) === code[index]){
-      alert("Yay!")
-      code++
+        index = 0;
+      }
+    } else { //restart index
+      index = 0;
     }
   }
-})
+  body.addEventListener('keydown', onKeyDownHandler)
+}
