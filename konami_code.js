567f53e1dd7ed2179852ff07d7465e9fc8c15628
hmelo1 <heriberto@melo.nyc>
2017-10-12 02:16:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,23 +2,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const body = document.querySelector('body')
 
 body.addEventListener('keydown', function(e){
-  function onKeyDownHandler(e){
-    const key = parseInt(e.detail || e.key)
-
-    if (key === code[index]){
-      code++
-      alert("Test111")
-
-      if (index === code.length){
-        alert("Test222")
-
-        index = 0;
-      }
-    }else {
-      index = 0;
-    }
+  key = parseInt(e.detail || e.key)
+  console.log(e.which)
+  if (key === code[0]){
+    alert("Yay!")
   }
-	console.log(e.which)
 })
 
 function init() {
@@ -42,4 +30,3 @@ function onKeyDownHandler(e){
     index = 0;
   }
 }
-
